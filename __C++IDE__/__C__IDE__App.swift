e1af3f094094b8c44839070af1b64f7ef9f9ff0c
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2025-03-04 19:06:22 +0600

    Initial Commit

---
+++ __C++IDE__/__C__IDE__App.swift
@@ -0,0 +1,17 @@
+//
+//  __C__IDE__App.swift
+//  __C++IDE__
+//
+//  Created by  Sadi on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct __C__IDE__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
