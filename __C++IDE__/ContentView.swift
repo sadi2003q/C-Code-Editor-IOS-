dd50127b30e0fb631e12676789147c8369522da4
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2025-03-04 19:20:09 +0600

    [First Commit]

--- __C++IDE__/ContentView.swift
+++ __C++IDE__/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var code = "//write your code here"
+    
+    @State private var output = "Output will be shown here"
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            TextEditor_Code
+            
+            View_ShowOutput
+            
         }
-        .padding()
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button_Run
+            }
+        }
+    }
+    
+    
+    private var TextEditor_Code: some View {
+        TextEditor(text: $code)
+            .font(.system(size: 18, design: .monospaced))
+            .frame(height: UIScreen.main.bounds.height*0.7)
+            .background(Color.black)
+            .foregroundStyle(.white)
+    }
+    
+    private var View_ShowOutput: some View {
+        Text(output)
+            .font(.system(size: 14, design: .monospaced))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
+            .background(.gray.opacity(0.8))
+            .foregroundStyle(.white)
+    }
+    
+    private var Button_Run: some View {
+        Button("Run", systemImage: "play") {
+            output="Running..."
+        }
     }
 }
 
